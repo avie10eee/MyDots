198dce0f6aabd3686fec69df0059c41b8a569d19
Geller Software <105246539+avie10eee@users.noreply.github.com>
2023-02-28 20:37:37 -0500

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
 
+echo " " | sudo sh -c 'echo "fastestmirror = True" >> /etc/dnf/dnf.conf'
+echo " " | sudo sh -c 'echo "max_parallel_downloads = 5 = True" >> /etc/dnf/dnf.conf'
+echo " " | sudo sh -c 'echo "defaultyes = True" >> /etc/dnf/dnf.conf'
+
+
 echo " " | sudo dnf upgrade
+
 
 mkdir /nix
 chown $USER /nix
@@ -8,8 +14,10 @@ chown $USER /nix
 #installing nix
 curl -L https://nixos.org/nix/install | sh -s -- --no-daemon
 
+
 #linking nix apps to usr/share/applications
 echo " " | sudo ln -s /nix/var/nix/profiles/per-user/$USER/profile/share/applications /usr/share/applications
+
 
 #enabling rpm-fuision
 echo " " | sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
@@ -19,8 +27,6 @@ echo " " | sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --e
 echo " " | sudo dnf groupupdate sound-and-video
 echo " " | sudo dnf install intel-media-driver
 
-#making cron readable
-echo " " | sudo chmod r+x /var/spool/cron/
 
 #crontab stuff(not sure if it works)
 echo "@reboot /~/$USER/startup.sh
@@ -35,6 +41,7 @@ git clone https://github.com/adi1090x/polybar-themes
 echo " " | sudo dnf install python tldr cmake curl wget git fzf tree ocs-url sl latte-dock.x86_64 fontawesome-fonts fontawesome-fonts-web polybar conky pulseaudio 
 
 #nix stuff(might work)
+touch after-reboot
 echo "vscodium, quickemu, virtmanager, pywal, Moe Dark Look-and-Feel(store.kde.org/p/1717596) , rpmfuision website, dnf.readthedocs.io/en/latest/conf_ref.html" > 
 
 
