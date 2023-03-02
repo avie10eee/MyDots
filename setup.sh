a02f08bc310786af30d1a5c54c77c9dcb1d40ee3
Geller Software <105246539+avie10eee@users.noreply.github.com>
2023-03-01 23:45:56 -0500

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+echo " " | sudo sh -c 'echo "#added for speed" >> /etc/dnf/dnf.conf'
 echo " " | sudo sh -c 'echo "fastestmirror = True" >> /etc/dnf/dnf.conf'
 echo " " | sudo sh -c 'echo "max_parallel_downloads = 5 = True" >> /etc/dnf/dnf.conf'
 echo " " | sudo sh -c 'echo "defaultyes = True" >> /etc/dnf/dnf.conf'
@@ -8,7 +9,11 @@ echo " " | sudo sh -c 'echo "defaultyes = True" >> /etc/dnf/dnf.conf'
 echo " " | sudo dnf upgrade
 
 
-mkdir /nix
+#install pkgs
+echo " " | sudo dnf install tldr cmake curl wget git fzf tree sl latte-dock fontawesome-fonts fontawesome-fonts-web polybar conky pulseaudio virt-manager qemu bash coreutils edk2-tools grep jq lsb procps python3 genisoimage usbutils util-linux sed spice-gtk-tools swtpm wget xdg-user-dirs xrandr unzip rofi
+
+
+sudo mkdir /nix
 chown $USER /nix
 
 #installing nix
@@ -26,6 +31,11 @@ echo " " | sudo dnf groupupdate core
 echo " " | sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 echo " " | sudo dnf groupupdate sound-and-video
 echo " " | sudo dnf install intel-media-driver
+echo " " | sudo dnf install ocs-url-3.1.0-1.fc20.x86_64.rpm
+
+#curl stuff: 1=moe-dark-look-and-feel-global-theme 2=colloid-full-icon-theme
+https://ocs-dl.fra1.cdn.digitaloceanspaces.com/data/files/1645543295/Moe-Dark.tar.gz?response-content-disposition=attachment%3B%2520Moe-Dark.tar.gz&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=RWJAQUNCHT7V2NCLZ2AL%2F20230302%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230302T034956Z&X-Amz-SignedHeaders=host&X-Amz-Expires=60&X-Amz-Signature=e01c061458c6e8bc5415cf37ed72207212622c18bc12b57aee9d075c44718b25
+https://ocs-dl.fra1.cdn.digitaloceanspaces.com/data/files/1639061356/Colloid-teal.tar.xz?response-content-disposition=attachment%3B%2520Colloid-teal.tar.xz&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=RWJAQUNCHT7V2NCLZ2AL%2F20230302%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230302T035348Z&X-Amz-SignedHeaders=host&X-Amz-Expires=60&X-Amz-Signature=8628932a39e5b7d2893f497ebd322c1e54475d78e13ccf56172990ec84f748eb
 
 
 #crontab stuff(not sure if it works)
@@ -34,20 +44,32 @@ echo "@reboot /~/$USER/startup.sh
 @reboot nix-channel --update; nix-env -iA nixpkgs.nix nixpkgs.cacert
 @reboot nix-collect-garbage" | crontab -e
 
+
+#git cloning
 git clone https://github.com/avie10eee/rofi
 git clone https://github.com/adi1090x/polybar-themes
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-#install pkgs
-echo " " | sudo dnf install python tldr cmake curl wget git fzf tree ocs-url sl latte-dock.x86_64 fontawesome-fonts fontawesome-fonts-web polybar conky pulseaudio 
 
-#nix stuff(might work)
-touch after-reboot
-echo "vscodium, quickemu, virtmanager, pywal, Moe Dark Look-and-Feel(store.kde.org/p/1717596) , rpmfuision website, dnf.readthedocs.io/en/latest/conf_ref.html" > 
+mv rofi ~/.config
+
+mkdir ~/.config/polybar
+mv polybar-themes/* ~/.config/
+echo "1" | sh ~/.config/polybar/setup.sh
+
+
+
+#after-reboot
+touch after-reboot.txt
+echo "nix-env -iA nixpkgs.quickemu nixpkgs.pywal nixpkgs.networkmanager_dmenu , git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" > after-reboot.sh
 
 
 #install zsh and ohmyzsh (PUT AT THE END)
 echo " " | sudo dnf install zsh
 echo "/bin/zsh" | sudo lchsh $USER
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sed -i 's/ZSH_THEME=""/ZSH_THEME="powerlevel10k/powerlevel10k"' ~/.zshrc
+
+echo " " | sudo reboot
 
 
