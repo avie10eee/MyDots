84c6fa49bd1b73ff8abc6cd146097b9d12e30e1a
avie10 <avie10eee@gmail.com>
2023-03-14 03:03:08 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/bash
 
+echo "##############################"
+echo "# Welcome to PigOS installer #"
+echo "##############################"
+
+
+echo "# Configuring DNF for speed #"
 echo " " | sudo sh -c 'echo "#added for speed" >> /etc/dnf/dnf.conf'
 echo " " | sudo sh -c 'echo "fastestmirror = True" >> /etc/dnf/dnf.conf'
 echo " " | sudo sh -c 'echo "max_parallel_downloads = 5 = True" >> /etc/dnf/dnf.conf'
 echo " " | sudo sh -c 'echo "defaultyes = True" >> /etc/dnf/dnf.conf'
 
-
+echo "# Upgrading your system #"
 echo " " | sudo dnf upgrade
 sleep 10
 
-#install pkgs
-echo " " | sudo dnf install tldr cmake curl tree sl latte-dock fontawesome-fonts fontawesome-fonts-web polybar virt-manager qemu bash coreutils edk2-tools grep jq lsb procps python3 genisoimage usbutils util-linux sed spice-gtk-tools swtpm wget xdg-user-dirs xrandr unzip brasero autojump neofetch alacritty micro tmux xclip kvantum bat
+echo "# Installing selected PKGs #"
+echo " " | sudo dnf install tldr cmake curl tree sl latte-dock fontawesome-fonts fontawesome-fonts-web polybar virt-manager qemu bash coreutils edk2-tools grep jq lsb procps python3 genisoimage usbutils util-linux sed spice-gtk-tools swtpm wget xdg-user-dirs xrandr unzip brasero autojump neofetch alacritty micro tmux xclip kvantum bat flameshot
 
 sleep 60
 
+echo "# Starting NIX Package Manager installation #"
 echo " " | sudo mkdir /nix
 echo " " | chown $USER /nix
 
@@ -26,6 +33,7 @@ sleep 60
 echo " " | sudo ln -s /nix/var/nix/profiles/per-user/$USER/profile/share/applications /usr/share/applications
 
 
+echo "# Enabling RPM-Fusion repositories #"
 #enabling rpm-fusion
 echo " " | sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 echo " " | sudo dnf groupupdate core
@@ -37,17 +45,18 @@ echo " " | sudo dnf install ocs-url-3.1.0-1.fc20.x86_64.rpm
 
 sleep 60
 
+# Kde stuff
 #curl stuff: 1=moe-dark-look-and-feel-global-theme 2=colloid-full-icon-theme
-curl https://ocs-dl.fra1.cdn.digitaloceanspaces.com/data/files/1645543295/Moe-Dark.tar.gz?response-content-disposition=attachment%3B%2520Moe-Dark.tar.gz&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=RWJAQUNCHT7V2NCLZ2AL%2F20230302%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230302T034956Z&X-Amz-SignedHeaders=host&X-Amz-Expires=60&X-Amz-Signature=e01c061458c6e8bc5415cf37ed72207212622c18bc12b57aee9d075c44718b25
-curl https://ocs-dl.fra1.cdn.digitaloceanspaces.com/data/files/1639061356/Colloid-teal.tar.xz?response-content-disposition=attachment%3B%2520Colloid-teal.tar.xz&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=RWJAQUNCHT7V2NCLZ2AL%2F20230302%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230302T035348Z&X-Amz-SignedHeaders=host&X-Amz-Expires=60&X-Amz-Signature=8628932a39e5b7d2893f497ebd322c1e54475d78e13ccf56172990ec84f748eb
+#curl https://ocs-dl.fra1.cdn.digitaloceanspaces.com/data/files/1645543295/Moe-Dark.tar.gz?response-content-disposition=attachment%3B%2520Moe-Dark.tar.gz&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=RWJAQUNCHT7V2NCLZ2AL%2F20230302%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230302T034956Z&X-Amz-SignedHeaders=host&X-Amz-Expires=60&X-Amz-Signature=e01c061458c6e8bc5415cf37ed72207212622c18bc12b57aee9d075c44718b25
+#curl https://ocs-dl.fra1.cdn.digitaloceanspaces.com/data/files/1639061356/Colloid-teal.tar.xz?response-content-disposition=attachment%3B%2520Colloid-teal.tar.xz&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=RWJAQUNCHT7V2NCLZ2AL%2F20230302%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230302T035348Z&X-Amz-SignedHeaders=host&X-Amz-Expires=60&X-Amz-Signature=8628932a39e5b7d2893f497ebd322c1e54475d78e13ccf56172990ec84f748eb
 
 #unzip curl stuff
-tar -xf Moe-Dark.tar.gz
-tar -xf Colloid-teal.tar.xz
+#tar -xf Moe-Dark.tar.gz
+#tar -xf Colloid-teal.tar.xz
 
 
 #crontab stuff(not sure if it works)
-
+echo "# Updating Cron jobs to update on reboot #"
 echo "@reboot echo " " | sudo dnf upgrade
 @reboot nix-channel --update; nix-env -iA nixpkgs.nix nixpkgs.cacert
 @reboot nix-collect-garbage
@@ -55,19 +64,22 @@ echo "@reboot echo " " | sudo dnf upgrade
 
 sleep 60
 
-mkdir ~/.config/polybar
+#uncomment if using kde
+#mkdir ~/.config/polybar
 
 #git cloning
-git clone https://github.com/adi1090x/polybar-themes "~/.config/polybar"
+#git clone https://github.com/adi1090x/polybar-themes "~/.config/polybar"
 git clone https://github.com/catppuccin/alacritty.git "~/.config/alacritty"
 
 
-echo "1" | sh ~/.config/polybar/polybar-themes/setup.sh
+#echo "1" | sh ~/.config/polybar/polybar-themes/setup.sh
+
+echo "# Adding Nerd fonts to ~/.fonts/truetype #"
+mkdir ~/.fonts && mkdir ~/.fonts/truetype
+mv ~/setup/fonts/* ~/.fonts/truetype
 
 
-mkdir ~/.fonts
-#curl -O https://objects.githubusercontent.com/github-production-release-asset-2e65be/27574418/068dcc12-38b5-4069-aba1-05fb6f0699cf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230306%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230306T062524Z&X-Amz-Expires=300&X-Amz-Signature=5c1a18684fa9cdec646f63172fc390093c0e3633ba3faf1791cd9ca4a1e6198f&X-Amz-SignedHeaders=host&actor_id=105246539&key_id=0&repo_id=27574418&response-content-disposition=attachment%3B%20filename%3DJetBrainsMono.zip&response-content-type=application%2Foctet-stream
-
+echo "# Adding micro configuration #"
 echo "{
     "autosave": 1,
     "hlsearch": true
@@ -80,6 +92,7 @@ echo "nix-env -iA nixpkgs.quickemu nixpkgs.pywal nixpkgs.tty-clock" > ~/after-re
 echo "set -g mouse on" >> ~/.tmux.conf
 
 
+echo "# Installing ZSH for Humans #"
 #install zsh4humans (PUT AT THE END)
 if command -v curl >/dev/null 2>&1; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
@@ -88,6 +101,6 @@ else
 fi
 
 
-echo Please Reboot!!
+echo "# Please Reboot!! #"
 
 
