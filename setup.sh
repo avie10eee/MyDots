cb177c6de3b59b08de7ab291720f4c720a63fa4a
avie10 <avie10eee@gmail.com>
2023-03-16 19:59:28 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,28 +1,33 @@
 #!/usr/bin/bash
 
+
+
+
 echo "##############################"
 echo "# Welcome to PigOS installer #"
 echo "##############################"
 sleep 2
+echo "please enter your password"
+read -sp "Password: " pass
 echo "# Starting install... #"
 sleep 3
 
 echo "# Configuring DNF for speed #"
-echo " " | sudo sh -c 'echo "#added for speed" >> /etc/dnf/dnf.conf'
-echo " " | sudo sh -c 'echo "fastestmirror = True" >> /etc/dnf/dnf.conf'
-echo " " | sudo sh -c 'echo "max_parallel_downloads = 5" >> /etc/dnf/dnf.conf'
-echo " " | sudo sh -c 'echo "defaultyes = True" >> /etc/dnf/dnf.conf'
+echo $pass | sudo sh -c 'echo "#added for speed" >> /etc/dnf/dnf.conf'
+echo $pass | sudo sh -c 'echo "fastestmirror = True" >> /etc/dnf/dnf.conf'
+echo $pass | sudo sh -c 'echo "max_parallel_downloads = 5" >> /etc/dnf/dnf.conf'
+echo $pass | sudo sh -c 'echo "defaultyes = True" >> /etc/dnf/dnf.conf'
 
 
 echo "# Upgrading your system #"
 sleep 4
-echo " " | sudo dnf upgrade
+echo $pass | sudo dnf upgrade
 
 sleep 5
 
 echo "# Installing selected PKGs #"
 sleep 4
-echo " " | sudo dnf install tldr make curl tree sl fontawesome-fonts fontawesome-fonts-web sed unzip neofetch alacritty micro tmux wl-clipboard bat flameshot opendoas kf5-krunner pipewire grim bluez Thunar firefox wget
+echo $pass | sudo dnf install tldr make curl tree sl fontawesome-fonts fontawesome-fonts-web sed unzip neofetch alacritty micro tmux wl-clipboard bat flameshot opendoas kf5-krunner pipewire grim bluez Thunar firefox wget
 
 #Installing ocs-url
 while true: do
@@ -30,7 +35,7 @@ while true: do
     case $yesno in
         [Yy]* ) 
             echo "Installing..."
-            echo " " | sudo dnf install ocs-url-3.1.0-1.fc20.x86_64.rpm
+            echo $pass | sudo dnf install ocs-url-3.1.0-1.fc20.x86_64.rpm
         ;;
         [Nn]* ) 
             echo "Aborted, skipping..."
@@ -49,12 +54,12 @@ while true: do
     case $nixinst in
         [Yy]* ) 
             echo "# Starting NIX Package Manager installation... #"
-            echo " " | sudo mkdir /nix
-            echo " " | chown $USER /nix
+            echo $pass | sudo mkdir /nix
+            echo $pass | chown $USER /nix
             curl -L https://nixos.org/nix/install | sh -s -- --no-daemon
             sleep 45
             #linking nix apps to usr/share/applications
-            echo " " | sudo ln -s /nix/var/nix/profiles/per-user/$USER/profile/share/applications /usr/share/applications
+            echo $pass | sudo ln -s /nix/var/nix/profiles/per-user/$USER/profile/share/applications /usr/share/applications
             echo "@reboot nix-channel --update; nix-env -iA nixpkgs.nix nixpkgs.cacert
             @reboot nix-collect-garbage" | crontab -e
         ;;
@@ -71,17 +76,17 @@ sleep 5
 echo "# Enabling RPM-Fusion repositories #"
 sleep 3
 #enabling rpm-fusion
-echo " " | sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-echo " " | sudo dnf groupupdate core
+echo $pass | sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+echo $pass| sudo dnf groupupdate core
 #multimedia and intel codecs
-echo " " | sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-echo " " | sudo dnf groupupdate sound-and-video
-echo " " | sudo dnf install intel-media-driver
+echo $pass | sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+echo $pass | sudo dnf groupupdate sound-and-video
+echo $pass | sudo dnf install intel-media-driver
 
 sleep 5
 
 #Hyprland
-#echo " " | sudo dnf install ninja-build cmake meson gcc-c++ libxcb-devel libX11-devel pixman-devel wayland-protocols-devel cairo-devel pango-devel
+#echo $pass | sudo dnf install ninja-build cmake meson gcc-c++ libxcb-devel libX11-devel pixman-devel wayland-protocols-devel cairo-devel pango-devel
 #git clone --recursive https://github.com/hyprwm/Hyprland
 
 #Installing ocs-url
@@ -90,7 +95,7 @@ while true: do
     case $hyprinst in
         [Yy]* ) 
             echo "Installing..."
-            echo " " | sudo dnf install ninja-build cmake meson gcc-c++ libxcb-devel libX11-devel pixman-devel wayland-protocols-devel cairo-devel pango-devel
+            echo $pass | sudo dnf install ninja-build cmake meson gcc-c++ libxcb-devel libX11-devel pixman-devel wayland-protocols-devel cairo-devel pango-devel
         ;;
         [Nn]* ) 
             echo "Aborted, skipping..."
@@ -105,8 +110,8 @@ sleep 2
 #crontab stuff(not sure if it works)
 echo "# Updating Cron jobs to update on reboot #"
 sleep 2
-echo "@reboot echo " " | sudo dnf upgrade
-@reboot echo " " | sudo dnf autoremove" | crontab -e
+echo "@reboot echo $pass | sudo dnf upgrade
+@reboot echo $pass | sudo dnf autoremove" | crontab -e
 
 sleep 5
 
@@ -171,7 +176,7 @@ else
 fi
 
 
-echo " " | sudo dnf autoremove
+echo $pass | sudo dnf autoremove
 
 echo "alias sudo="doas"" >> .zshrc
 echo "neofetch" >> .zshrc
