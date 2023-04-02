f1763ee4a7656cafe08c39efcad15d1147a24ab9
avie10 <avie10eee@gmail.com>
2023-04-02 03:25:41 -0400

    changed everything to functions, massive changes

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,80 +1,162 @@
 #!/bin/bash
 
+
+#vars
 DIR={$HOME}/setup
 NERDF=https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/JetBrainsMono.zip
-
-echo "################################"
-echo "# this is post PigOS installer #"
-echo "################################"
+PS3='Would you like to install any's: '
+options=("SpectrWM" "CWM" "Qtile" "Hyprland" "AwesomeWM" "Skip")
 
 
-echo "# Adding micro configuration #"
-echo '{
-    "autosave": 1,
-    "hlsearch": true
-    "colorscheme": "gruvbox"
-}' > ${HOME}/.config/micro/settings.json
+#functions
+welcome () {
+    echo "################################"
+    echo "# this is post PigOS installer #"
+    echo "################################"
+}
 
-micro -plugin install detectindent manipulator filemanager quoter
+micro_conf () {
 
+    echo "# Adding micro configuration #"
 
+    mkdir -p ${HOME}/.config/micro/colorschemes
+    mv ${DIR}catppuccin-mocha.micro ${HOME}/.config/micro/colorschemes
 
-#tmux config
-echo "set -g mouse on" >> ${HOME}/.tmux.conf
+    echo '{
+        "autosave": 1,
+        "hlsearch": true
+        "colorscheme": "gruvbox"
+    }' > ${HOME}/.config/micro/settings.json
 
-#zshrc
-echo "Configuring zshrc"
+    #plugins
+    micro -plugin install detectindent manipulator filemanager quoter
+}
 
-echo "${HOME}/setup/.zshrc" >> .zshrc
+tmux_conf () {
+    #turning on mouse in tmux
+    echo "set -g mouse on" >> ${HOME}/.tmux.conf
+}
 
+zsh_conf () {
 
-mkdir .wallpapers
-cp "${DIR}/wallpapers" ".wallpapers"
-mv "${DIR}/setup/autobg.sh" ${HOME}
+    echo "Configuring zshrc"
+    #p10k
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+    echo "${HOME}/setup/.zshrc" >> .zshrc
 
-read -p "Would you like configure DT's colorscripts Y/N " colorsc
-if [ "$colorsc" = 'y' ]; then
-    echo "# Configuring DT's colorscripts"
+    #adding zsh plugins
+    sed -i 's/ZSH_THEME=""/ZSH_THEME="powerlevel10k/powerlevel10k/"' ${HOME}/.zshrc
+    sed -i 's/plugins=(git)/plugins=(zsh-syntax-highlighting zsh-autosuggestions)/' .zshrc
+
+}
+
+wallpaper () {
+
+    mkdir .wallpapers
+    cp "${DIR}/wallpapers" ".wallpapers"
+    mv "${DIR}/setup/autobg.sh" ${HOME}
+}
+
+colorscript_conf () {
+
+    read -p "Have you installed DT's colorscripts Y/N " colorsc
+    if [ "$colorsc" = 'y' ]; then
+        echo "# Configuring DT's colorscripts"
+        sleep 2
+        colorscript -b xmonad
+        colorscript -b tiefighter2
+        colorscript -b tiefighter1row
+        colorscript -b tifighter1
+        colorscript -b thebat2
+        colorscript -b spectrum
+        colorscript -b pukeskull
+        colorscript -b mouseface2
+        colorscript -b guns
+        colorscript -b colorbars
+        colorscript -b bloks
+        colorscript -b blok
+    fi
+}
+
+nerd_font () {
+    read -p "Would you like to install JetBrainsMono nerd font Y/N " fontinst
+    case $fontinst in
+        y|Y ) echo "# Adding Nerd fonts to ${HOME}/.fonts/truetype #"; 
+        mkdir -p ${HOME}/.fonts/truetype; wget -q ${NERDF}; 
+        unzip "${HOME}/JetBrainsMono.zip" -d "${HOME}/.fonts/truetype"; 
+        fc-cache;;
+
+        n|N ) echo "Aborted, skipping...";;
+    esac
+}
+
+doas_conf () {
+    echo "Configuring doas"
+    echo "add the following to /etc/doas.conf" > doas.txt
+    echo "permit persist keepenv ${USER} as root" >> doas.txt
+}
+
+wms_inst () {
+    select opt in "${options[@]}"
+    do
+        case $opt in
+            "SpectrWM")
+                nix-env -iA nixpkgs.spectrwm
+                ;;
+            "CWM")
+                nix-env -iA nixpkgs.cwm
+                ;;
+            "Qtile")
+                nix-env -iA nixpkgs.qtile
+                ;;
+            "Hyprland")
+                nix-env -iA nixpkgs.hyprland
+                ;;
+            "AwesomeWM")
+            nix-env -iA nixpkgs.awesome
+            "Skip")
+                break
+                ;;
+            *) echo "invalid option";;
+        esac
+    done
+}
+
+picom () {
+    read -p "Would you like to install Picom Y/N " jona
+    if [ "$jona" = 'y' ]; then
+        nix-env -iA mixpkgs.picom-jonaburg
+}
+
+gooodbye () {
+    echo "Thank you for using the PigOS script"
     sleep 2
-    colorscript -b xmonad
-    colorscript -b tiefighter2
-    colorscript -b tiefighter1row
-    colorscript -b tifighter1
-    colorscript -b thebat2
-    colorscript -b spectrum
-    colorscript -b pukeskull
-    colorscript -b mouseface2
-    colorscript -b guns
-    colorscript -b colorbars
-    colorscript -b bloks
-    colorscript -b blok
-fi
+    echo "I would recommend rebooting"
+    sleep 5
+}
 
-
-
-mkdir -p ${HOME}/.config/micro/colorschemes
-mv ${DIR}catppuccin-mocha.micro ${HOME}/.config/micro/colorschemes
-#ADD THE COLORSCHEME CHANGE!!!!!!!!
-
-
-read -p "Would you like to install JetBrainsMono nerd font Y/N " fontinst
-case $fontinst in
-    y|Y ) echo "# Adding Nerd fonts to ${HOME}/.fonts/truetype #"; mkdir -p ${HOME}/.fonts/truetype; wget -q ${NERDF}; unzip "${HOME}/JetBrainsMono.zip" -d "${HOME}/.fonts/truetype"; fc-cache;;
-    n|N ) echo "Aborted, skipping...";;
-esac
-
-read -p "Do you want to install some WM's with Nix Package Manager Y/N " fontinst
-case $fontinst in
-    y|Y ) echo "Ok, Installing... "; nix-env -iA nixpkgs.hyprland nixpkgs.awesome nixpkgs.qtile nixpkgs.cwm nixpkgs.spectrwm nixpkgs.picom-jonaburg;;
-    n|N ) echo "Aborted, skipping...";;
-esac
-
-
-sed -i 's/ZSH_THEME=""/ZSH_THEME="powerlevel10k/powerlevel10k/"' ${HOME}/.zshrc
-sed -i 's/plugins=(git)/plugins=(zsh-syntax-highlighting zsh-autosuggestions)/' .zshrc
-
-#doas
-echo "Configuring doas"
-echo "add the following to /etc/doas.conf" > doas.txt
-echo "permit persist keepenv ${USER} as root" >> doas.txt
+#running functions
+welcome
+sleep 2
+micro_conf
+sleep 2
+tmux_conf
+sleep 2
+zsh_conf
+sleep 2
+wallpaper
+sleep 2
+colorscript_conf
+sleep 2
+nerd_font
+sleep 2
+doas_conf
+sleep 2
+wms_inst
+sleep 2
+picom
+sleep 2
+gooodbye
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,191 +1,238 @@
 #!/bin/bash
 
 
+#vars
 DIR=${HOME}/setup
 
-echo "##############################"
-echo "# Welcome to PigOS installer #"
-echo "##############################"
 
+#functions
+welcome () {
+
+    echo "##############################"
+    echo "# Welcome to PigOS installer #"
+    echo "##############################"
+}
+
+passinput () {
+
+    echo "please enter your password"
+    read -sp "Password: " pass
+    echo
+    echo "# Starting install... #"
+}
+
+dnf_config () {
+
+    echo "# Configuring DNF for speed #"
+    echo "$pass" | sudo sh -c 'echo "#added for speed" >> /etc/dnf/dnf.conf'
+    echo "$pass" | sudo sh -c 'echo "fastestmirror = True" >> /etc/dnf/dnf.conf'
+    echo "$pass" | sudo sh -c 'echo "max_parallel_downloads = 5" >> /etc/dnf/dnf.conf'
+    echo "$pass" | sudo sh -c 'echo "defaultyes = True" >> /etc/dnf/dnf.conf'
+    
+    echo "# Upgrading your system #"
+    sleep 2
+    echo "$pass" | sudo dnf upgrade
+}
+
+pkg_list () {
+
+    echo "# Installing selected PKGs #"
+
+    sleep 2
+
+    #misc
+    echo "$pass" | sudo dnf install opendoas greetd rust cargo lxqt-archiver conky rofi gccxfce4-power-manager volumeicon xfce4-settings xfce4-power-manager nitrogen dunst
+
+    #font-awesome
+    echo "$pass" | sudo dnf install polybar fontawesome-fonts fontawesome5-fonts fontawesome-fonts-web 
+
+    #term
+    echo "$pass" | sudo dnf install alacritty
+
+    #bluetooth + audio
+    echo "$pass" | sudo dnf install bluez bluez-tools bluez-libs blueman pulseaudio pavucontrol easyeffects pulseaudio-module-bluetooth 
+
+    #term tools +term editor + gui editor
+    echo "$pass" | sudo dnf install micro tmux bat wget sed unzip neofetch curl tldr make tree exa acpi cmake ninja-build geany feh meson sxhkd 
+
+    #screenshot
+    echo "$pass" | sudo dnf install flameshot 
+
+    #browser + file manager
+    echo "$pass" | sudo dnf install firefox thunar
+
+    echo "# Finished packages installation #"
+
+}
+
+picom_deps () {
+
+    read -p "Would you like to install picom dependecies Y/N " picom
+    case $picom in
+        y|Y ) echo "Installing..."; 
+        echo "$pass" | sudo dnf install dbus-devel gcc git libconfig-devel libdrm-devel libev-devel libX11-devel libX11-xcb libXext-devel libxcb-devel libGL-devel libEGL-devel meson pcre2-devel pixman-devel uthash-devel xcb-util-image-devel xcb-util-renderutil-devel xorg-x11-proto-devel;;
+        n|N ) echo "Aborted, skipping...";;
+    esac
+}
+
+colorscripts_inst () {
+
+    read -p "Would you like to use DT's colorscripts Y/N " colorsc
+    case $colorsc in
+        y|Y ) echo "Installing..."; 
+        echo "$pass" | sudo dnf copr enable foopsss/shell-color-scripts; 
+        echo "$pass" | sudo dnf install shell-color-scripts;;
+        n|N ) echo "Aborted, skipping...";;
+    esac
+}
+
+wayland_deps () {
+
+    read -p "Would you like to install wayland packages (why not?) Y/N " wayl
+    case $yesno in
+        y|Y ) echo "Installing..."; echo "$pass" | sudo dnf install wl-clipboard pipewire grim swaybg swayidle swaylock wlroots waybar wofi foot mako slurp wf-recorder light yad viewnior imagemagick xfce-polkit xorg-xwayland xdg-desktop-portal-wlr qt5-wayland qt6-wayland wireplumber easyeffects;;
+        n|N ) echo "Aborted, skipping...";;
+    esac
+}
+
+ocs-ur () {
+    #Installing ocs-url
+    read -p "Would you like to install ocs-url (recommended) Y/N " yesno
+    case $yesno in
+        y|Y ) echo "Installing..."; echo "$pass" | sudo dnf install ocs-url-3.1.0-1.fc20.x86_64.rpm;;
+        n|N ) echo "Aborted, skipping...";;
+    esac
+}
+
+nix_inst () {
+
+    #Nix install
+    read -p "would you like to install NIX Package Manager Y/N " nixinst
+    case $nixinst in
+        y|Y ) echo "# Starting NIX Package Manager installation... #";
+            echo "$pass" | sudo mkdir /nix;
+            sudo chown "$USER" /nix;
+            curl -L https://nixos.org/nix/install | sh -s -- --no-daemon;
+            sleep 45;
+            #linking nix apps to usr/share/applications
+            echo "$pass" | sudo ln -s /nix/var/nix/profiles/per-user/${HOME}/profile/share/applications /usr/share/applications;
+            #adding nix stuff to cron
+            echo ${cron} > cfile;
+            echo "@reboot nix-channel --update; nix-env -iA nixpkgs.nix nixpkgs.cacert" >> cfile;
+            echo "@reboot nix-collect-garbage" >> cfile;;
+        n|N ) echo "Aborted, skipping...";;
+    esac
+}
+
+rpm_fusion () {
+
+    echo "# Enabling RPM-Fusion repositories #"
+    sleep 3
+    #enabling rpm-fusion
+    echo "$pass" | sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    echo "$pass"| sudo dnf groupupdate core
+    #multimedia and intel codecs
+    echo "$pass" | sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+    echo "$pass" | sudo dnf groupupdate sound-and-video
+    echo "$pass" | sudo dnf install intel-media-driver
+}
+
+crontab () {
+    #crontab stuff
+    echo "# Updating Cron jobs to update on reboot #"
+    sleep 2
+    echo "@reboot echo $pass | sudo dnf upgrade" >> cfile
+    echo "@reboot echo $pass | sudo dnf autoremove" >> cfile
+    echo "@hourly ./.autobg" >> cfile
+}
+
+polybar () {
+
+    mkdir ${HOME}/.config/polybar
+    #cloning polybar-themes by aditya shakya
+    git clone https://github.com/adi1090x/polybar-themes "$HOME/.config/polybar"
+    #running polybar-themes installer
+    sh ${HOME}/.config/polybar/polybar-themes/setup.sh
+}
+
+neofetch_conf () {
+    #moving neofetch config to .config
+    mv ${DIR}/neofetch/config.conf ${HOME}/.config/neofetch
+}
+
+alacritty_conf () {
+
+    mkdir -p ${HOME}/.config/alacritty
+    mv ${DIR}/alacritty.yml ${HOME}/.config/alacritty
+}
+
+wm_dots () {
+    #adding arco wm dots to .config
+    mv ${DIR}/wmdots/* ${HOME}
+    mv ${HOME}/'spectrwm' 'qtile'  'awesome' 'cwm' .config
+}
+
+zsh_inst () {
+
+    echo " " | sudo dnf install zsh
+    echo "/bin/zsh" | sudo lchsh "$USER"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
+
+sysctl_stuff () {
+
+    sudo systemctl enable bluetooth
+    sudo systemctl enable acpid
+}
+
+cronmerge () {
+
+    crontab cfile
+}
+
+cleanup () {
+    echo "# Running cleanup #"
+    echo "$pass" | sudo dnf autoremove
+}
+
+
+#running functions
+welcome
 sleep 2
-
-echo
-
-echo "please enter your password"
-read -sp "Password: " pass
-echo
-echo "# Starting install... #"
-
-sleep 3
-
-echo
-
-echo "# Configuring DNF for speed #"
-echo "$pass" | sudo sh -c 'echo "#added for speed" >> /etc/dnf/dnf.conf'
-echo "$pass" | sudo sh -c 'echo "fastestmirror = True" >> /etc/dnf/dnf.conf'
-echo "$pass" | sudo sh -c 'echo "max_parallel_downloads = 5" >> /etc/dnf/dnf.conf'
-echo "$pass" | sudo sh -c 'echo "defaultyes = True" >> /etc/dnf/dnf.conf'
-
-
-echo "# Upgrading your system #"
-
+passinput
 sleep 2
-
-echo "$pass" | sudo dnf upgrade
-
+dnf_config
 sleep 2
-
-echo "# Installing selected PKGs #"
-
+pkg_list
 sleep 2
-
-#misc
-echo "$pass" | sudo dnf install opendoas greetd rust cargo lxqt-archiver conky rofi gccxfce4-power-manager volumeicon xfce4-settings xfce4-power-manager nitrogen dunst
-
-#font-awesome
-echo "$pass" | sudo dnf install polybar fontawesome-fonts fontawesome5-fonts fontawesome-fonts-web 
-
-#term
-echo "$pass" | sudo dnf install alacritty
-
-#bluetooth + audio
-echo "$pass" | sudo dnf install bluez bluez-tools bluez-libs blueman pulseaudio pavucontrol easyeffects pulseaudio-module-bluetooth 
-
-#term tools +term editor + gui editor
-echo "$pass" | sudo dnf install micro tmux bat wget sed unzip neofetch curl tldr make tree exa acpi cmake ninja-build geany feh meson sxhkd 
-
-#screenshot
-echo "$pass" | sudo dnf install flameshot 
-
-#browser + file manager
-echo "$pass" | sudo dnf install firefox thunar
-
-echo "# Finished packages installation #"
+picom_deps
 sleep 2
-
-read -p "Would you like to install picom dependecies Y/N " picom
-case $picom in
-    y|Y ) echo "Installing..."; 
-    echo "$pass" | sudo dnf install dbus-devel gcc git libconfig-devel libdrm-devel libev-devel libX11-devel libX11-xcb libXext-devel libxcb-devel libGL-devel libEGL-devel meson pcre2-devel pixman-devel uthash-devel xcb-util-image-devel xcb-util-renderutil-devel xorg-x11-proto-devel;;
-    n|N ) echo "Aborted, skipping...";;
-esac
-
-
-read -p "Would you like to use DT's colorscripts Y/N " colorsc
-case $colorsc in
-    y|Y ) echo "Installing..."; 
-    echo "$pass" | sudo dnf copr enable foopsss/shell-color-scripts; 
-    echo "$pass" | sudo dnf install shell-color-scripts;;
-    n|N ) echo "Aborted, skipping...";;
-esac
-
+colorscripts_inst
 sleep 2
-
-
-read -p "Would you like to install wayland packages (why not?) Y/N " wayl
-case $yesno in
-    y|Y ) echo "Installing..."; echo "$pass" | sudo dnf install wl-clipboard pipewire grim swaybg swayidle swaylock wlroots waybar wofi foot mako slurp wf-recorder light yad viewnior imagemagick xfce-polkit xorg-xwayland xdg-desktop-portal-wlr qt5-wayland qt6-wayland wireplumber easyeffects;;
-    n|N ) echo "Aborted, skipping...";;
-esac
-
-
+wayland_deps
 sleep 2
-
-#Installing ocs-url
-read -p "Would you like to install ocs-url (recommended) Y/N " yesno
-case $yesno in
-    y|Y ) echo "Installing..."; echo "$pass" | sudo dnf install ocs-url-3.1.0-1.fc20.x86_64.rpm;;
-    n|N ) echo "Aborted, skipping...";;
-esac
-
-
-sleep 5
-
-#Nix install
-read -p "would you like to install NIX Package Manager Y/N " nixinst
-case $nixinst in
-    y|Y ) echo "# Starting NIX Package Manager installation... #";
-        echo "$pass" | sudo mkdir /nix;
-        sudo chown "$USER" /nix;
-        curl -L https://nixos.org/nix/install | sh -s -- --no-daemon;
-        sleep 45;
-        #linking nix apps to usr/share/applications
-        echo "$pass" | sudo ln -s /nix/var/nix/profiles/per-user/${HOME}/profile/share/applications /usr/share/applications;
-        #adding nix stuff to cron
-        echo ${cron} > cfile;
-        echo "@reboot nix-channel --update; nix-env -iA nixpkgs.nix nixpkgs.cacert" >> cfile;
-        echo "@reboot nix-collect-garbage" >> cfile;;
-    n|N ) echo "Aborted, skipping...";;
-esac
-
-sleep 5
-
-echo
-
-echo "# Enabling RPM-Fusion repositories #"
-sleep 3
-#enabling rpm-fusion
-echo "$pass" | sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-echo "$pass"| sudo dnf groupupdate core
-#multimedia and intel codecs
-echo "$pass" | sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-echo "$pass" | sudo dnf groupupdate sound-and-video
-echo "$pass" | sudo dnf install intel-media-driver
-
-sleep 5
-
-#crontab stuff
-echo "# Updating Cron jobs to update on reboot #"
+ocs-ur
 sleep 2
-echo "@reboot echo $pass | sudo dnf upgrade" >> cfile
-echo "@reboot echo $pass | sudo dnf autoremove" >> cfile
-echo "@hourly ./.autobg" >> cfile
-
-sleep 5
-
-mkdir ${HOME}/.config/polybar
-
-#git cloning
-git clone https://github.com/adi1090x/polybar-themes "$HOME/.config/polybar"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlig
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-#running polybar-themes installer
-sh ${HOME}/.config/polybar/polybar-themes/setup.sh
-
-#neofetch
-mv ${DIR}/neofetch/config.conf ${HOME}/.config/neofetch
-
-mkdir -p ${HOME}/.config/alacritty
-mv ${DIR}/alacritty.yml ${HOME}/.config/alacritty
-
-
-
-#wm's
-mv ${DIR}/wmdots/* ${HOME}
-mv ${HOME}/'spectrwm' 'qtile'  'awesome' 'cwm' .config
-
-
-#install zsh and ohmyzsh (PUT AT THE END)
-echo " " | sudo dnf install zsh
-echo "/bin/zsh" | sudo lchsh "$USER"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-sleep 3
-
-#systemctl stuff
-sudo systemctl enable bluetooth
-sudo systemctl enable acpid
-
-
-#adding all changes to crontab
-crontab cfile
-
-echo "# Running cleanup #"
-echo "$pass" | sudo dnf autoremove
-
+nix_inst
+sleep 2
+rpm_fusion
+sleep 2
+crontab
+sleep 2
+polybar
+sleep 2
+neofetch_conf
+sleep 2
+alacritty_conf
+sleep 2
+wm_dots
+sleep 2
+zsh_inst
+sleep 2
+sysctl_stuff
+sleep 2
+cronmerge
+sleep 2
+cleanup
 
 echo "# Please Reboot!! #"
-
-sleep 8
