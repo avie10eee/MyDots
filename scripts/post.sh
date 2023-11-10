ce86b8a3090cd1949436e0f66ac5246c0240cbeb
Avi geller <avie10@proton.me>
2023-11-09 20:01:45 -0500

    removed some files andupdated post.sh removed crud

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
+#check if user is root
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run as root."
+    exit 1
+fi
+
 
 #vars
-#setting the dir
-DIR="{$HOME}/setup"
 
 #nerd font
 NERDF="https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/JetBrainsMono.zip"
 
 #functions
+
+checks () {
+    if ! command -v git &> /dev/null; then
+        echo "Git is not installed. Please install it and try again."
+        exit 1
+    fi
+
+    
+    if ! command -v wget &> /dev/null; then
+    echo "Wget is not installed. Please install it and try again."
+    return
+    fi
+
+    if ! command -v unzip &> /dev/null; then
+    echo "Unzip is not installed. Please install it and try again."
+    return
+    fi
+
+}
+
+
 #welcomes the user
 welcome () {
     echo "################################"
@@ -19,12 +44,16 @@ welcome () {
 #configures micro text editor
 micro_conf () {
 
+    if [ ! -d "${HOME}/.config/micro" ]; then
+        mkdir -p "${HOME}/.config/micro"
+    fi
+
     echo "# Adding micro configuration #"
 
     echo '{
         "autosave": 1,
         "hlsearch": true,
-        "colorscheme": "gruvbox",
+        "colorscheme": "gruvbox"
     }' > "${HOME}"/.config/micro/settings.json
 
     #plugins
@@ -44,6 +73,15 @@ tmux_conf () {
 
 #configures zshell
 zsh_conf () {
+
+    if [ ! -d "${HOME}/.oh-my-zsh/custom/themes" ]; then
+    mkdir -p "${HOME}/.oh-my-zsh/custom/themes"
+    fi
+
+    if [ ! -d "${HOME}/.oh-my-zsh/custom/plugins" ]; then
+    mkdir -p "${HOME}/.oh-my-zsh/custom/plugins"
+    fi
+
     echo "Configuring zshrc"
 
     #p10k
@@ -54,34 +92,33 @@ zsh_conf () {
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
     if [ -f "${HOME}/setup/.zshrc" ]; then
-    echo "${HOME}/setup/.zshrc" >> .zshrc
+    echo "${HOME}/setup/.zshrc" >> "${HOME}/.zshrc"
     else
-    echo "${HOME}/setup/.zshrc" > .zshrc
+    echo "${HOME}/setup/.zshrc" > "${HOME}/.zshrc"
     fi
 
     #adding zsh plugins
     sed -i 's|ZSH_THEME=""|ZSH_THEME="powerlevel10k/powerlevel10k"|' ${HOME}/.zshrc
-    sed -i 's/plugins=(git)/plugins=(zsh-syntax-highlighting zsh-autosuggestions)/' .zshrc
+    sed -i 's/plugins=(git)/plugins=(zsh-syntax-highlighting zsh-autosuggestions)/' ${HOME}/.zshrc
 
 }
 
 #installs jetbrains mono nerd font
 nerd_font () {
+
+    if [ ! -d "${HOME}/.fonts/truetype" ]; then
+    mkdir -p "${HOME}/.fonts/truetype"
+    fi
+
+
     read -p "Would you like to install JetBrainsMono nerd font Y/N " fontinst
     if [ $fontinst = 'y' ]; then
     echo "# Adding Nerd fonts to system #"
-    mkdir -p ${HOME}/.fonts/truetype
-    wget -q ${NERDF}
+    wget -q ${NERDF} -P "${HOME}/Downloads/"
     unzip "${HOME}/Downloads/JetBrainsMono.zip" -d "${HOME}/.fonts/truetype"
     fc-cache
-    fi
-}
-
-#install a picom fork with blur and rounded corners
-picom () {
-    read -p "Would you like to install Picom Y/N " jona
-    if [ "$jona" = 'y' ]; then
-        nix-env -iA nixpkgs.picom-jonaburg
+    else
+    echo "Skipping Nerd fonts installation"
     fi
 }
 
@@ -97,6 +134,8 @@ gooodbye () {
 #main
 main () {
 
+    checks
+    sleep 2
     welcome
     sleep 2
     micro_conf
@@ -106,8 +145,6 @@ main () {
     zsh_conf
     sleep 2
     nerd_font
-    sleep 2
-    picom
     sleep 2
     gooodbye
     sleep 6
